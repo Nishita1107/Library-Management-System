bf8ce1d66406721a5ae756613974430a79997a21
Nishita Shinde <nishitadms@gmail.com>
2026-09-01 10:48:37 +0530

    Add files via upload

---
+++ Book.js
@@ -0,0 +1,20 @@
+const mongoose = require("mongoose");
+
+const BookSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: true
+  },
+
+  author: {
+    type: String,
+    required: true
+  },
+
+  quantity: {
+    type: Number,
+    required: true
+  }
+});
+
+module.exports = mongoose.model("Book", BookSchema);
